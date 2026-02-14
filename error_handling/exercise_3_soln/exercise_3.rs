3d0ce461e36a305fce17c155e878e06c862f885b
mohamed-aslam-56 <mohamed.aslam@blubridge.com>
2026-02-14 19:04:51 +0530

    Exercise 3 better solution

--- error_handling/exercise_3_soln/exercise_3.rs
+++ error_handling/exercise_3_soln/exercise_3.rs
@@ -2,9 +2,10 @@ use std::fs::{self,File};
 use std::error::Error;
 use std::io::ErrorKind;
 
-fn main(){
-    let res=read_username_from_file();
-    println!("{:?}",res);
+fn main()->Result<(),Box<dyn Error>>{
+    let username=read_username_from_file();
+    println!("Username {:?}",username);
+    Ok(())
 }
 fn read_username_from_file()->Result<String,Box<dyn Error>>{
     let path="config.txt";
@@ -13,25 +14,18 @@ fn read_username_from_file()->Result<String,Box<dyn Error>>{
     match file{
         Err(error)=>match error.kind(){
             ErrorKind::NotFound=>{
-                let mut newFile=File::create(&path);
-                match newFile{
-                    Err(e)=>Err(e.into()),
-                    Ok(fc)=>{
-                        let word="guest".to_string();
-                        fs::write(path,&word);
-                        Ok(word)
-                    }
-                }
+                let mut newFile=File::create(&path)?;
+                let word="guest".to_string();
+                fs::write(path,&word)?;
+                Ok(word)
             }
             _=>Err("File cannot be opened".into())
         }
 
         Ok(fc)=>{
-            let content=fs::read_to_string(path);
-            match content{
-                Ok(word)=>Ok(word),
-                Err(e)=>Err("File is not available".into())
-            }
+            let content=fs::read_to_string(path)?;
+            Ok(content)
+            
         }
         
     }
